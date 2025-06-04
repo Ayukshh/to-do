c6e4ee2f759cb3d88f43824ae462186aa11c4d01
ayush <ayukshbhattarai@gmail.com>
2025-06-04 08:07:50 +0545

    commiting all

---
+++ to-do/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Todo from './todo.jsx';
+
+function App() {
+  return (
+    <>
+      <Todo />
+    </>
+  );
+}
+
+export default App;
---
+++ to-do/src/todo.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+function Todo() {
+    const [tasks, setTasks] = useState([{ id: 1, text: '', isChecked: false }]);
+
+    function handleChange(e, id) {
+        setTasks((prevTasks) =>
+            prevTasks.map((task) =>
+                task.id === id ? { ...task, text: e.target.value } : task
+            )
+        );
+    }
+
+    function handleCheckboxChange(id) {
+        setTasks((prevTasks) =>
+            prevTasks.map((task) =>
+                task.id === id ? { ...task, isChecked: !task.isChecked } : task
+            )
+        );
+    }
+
+    function handleRemoveTask(id) {
+        setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
+    }
+
+    function handleDuplicate() {       //add tasks
+        setTasks((prevTasks) => [
+            ...prevTasks,
+            { id: Date.now(), text: '', isChecked: false }
+        ]);
+    }
+
+    return (
+        <>
+            {tasks.map((task) => (
+                <div key={task.id} style={{ marginBottom: "10px" }}>
+                    <input
+                        type="checkbox"
+                        checked={task.isChecked}
+                        onChange={() => handleCheckboxChange(task.id)}
+                    />
+                    <input
+                        value={task.text}
+                        onChange={(e) => handleChange(e, task.id)}
+                        style={{
+                            padding: "8px",
+                            border: "1px solid #ccc",
+                            color: task.isChecked ? "gray" : "white"
+                        }}
+                    />
+                    <button onClick={() => handleRemoveTask(task.id)}>delete</button>
+                </div>
+            ))}
+            <button onClick={handleDuplicate}>Add Tasks</button>
+        </>
+    );
+}
+
+export default Todo;
